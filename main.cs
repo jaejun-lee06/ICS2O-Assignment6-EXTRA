0a982b062b543c0b9f6373f5dd8cc6ba7e792e30
Jaejun Lee <99355321+jaejun-lee06@users.noreply.github.com>
2022-06-25 05:18:10 +0000

    done

--- main.cs
+++ main.cs
@@ -1,7 +1,29 @@
-using System;
+// Created by: Jaejun Lee
+// Created on: June 2022
+//
+// This program accepts user input
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+class Program
+{
+    public static async Task Main(string[] args)
+    {
+        HttpClient client = new HttpClient();
+        string response = await client.GetStringAsync(
+            "https://api.thecatapi.com/v1/images/search"
+        );
+        var json = System.Text.Json.JsonSerializer.Deserialize<Object>(response);
+        JsonNode jsonData = JsonNode.Parse(response)!;
+        JsonNode url = jsonData[0]["url"];
+
+        Console.WriteLine("Random Cat picture " + url + " .");
+        Console.WriteLine("\nDone");
+    }
 }
